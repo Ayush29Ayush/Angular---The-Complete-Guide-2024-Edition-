915f624216e6863b8d6a550270613f04c7eebdb4
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-28 12:42:13 +0530

    feat: Header Component added

---
+++ first-angular-app/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-header', //! selector is used to select this component as a HTML element tag in the HTML file
+    standalone: true, //! standalone is used to tell Angular that this component is a standalone component and not a module component
+    //! template: '<h1>Hello Angular</h1>',
+    templateUrl: './header.component.html', //! templateUrl is used to specify the location of the template file
+    //! styles: ['h1 { color: red }'],
+    styleUrl: './header.component.css' //! styleUrl is used to specify the location of the CSS file
+})
+
+export class HeaderComponent {}
