f42ab60e7aec8820f41209a28faf5ff20a57409b
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-30 03:12:29 +0530

    feat: Dynamic data content

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,6 +10,17 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+//! Variables outside the export class component needs data type to be defined like let vat const but inside the export class component, just type the variable name.
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex]; // Advantage of doing this is that we can now access this selectedUser inside the template of this component.
+
+  // private selectedUser = DUMMY_USERS[randomIndex]; //! Setting it private means this is accessible only inside this class and not in template
+  // public selectedUser = DUMMY_USERS[randomIndex]; //! Setting it public means this is accessible in template
+
+  //! This is a getter
+  get imagePath() {
+    // return 'assets/users/' + this.selectedUser.avatar
+    return `assets/users/${this.selectedUser.avatar}`
+  }
 }
