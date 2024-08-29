184a2312ab2b6607509874adacaffd6a49312396
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-30 04:05:49 +0530

    feat: using singals to get dynamic data

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,16 +11,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 
-//! Variables outside the export class component needs data type to be defined like let vat const but inside the export class component, just type the variable name.
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex]; // Advantage of doing this is that we can now access this selectedUser inside the template of this component.
-
-  // private selectedUser = DUMMY_USERS[randomIndex]; //! Setting it private means this is accessible only inside this class and not in template
-  // public selectedUser = DUMMY_USERS[randomIndex]; //! Setting it public means this is accessible in template
+  // selectedUser = DUMMY_USERS[randomIndex]; // Advantage of doing this is that we can now access this selectedUser inside the template of this component.
+  selectedUser = signal(DUMMY_USERS[randomIndex]); //! using signals for state management
+  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`); //! using computed for state management which uses signals under the hood
 
   //! This is a getter
-  get imagePath() {
-    // return 'assets/users/' + this.selectedUser.avatar
-    return `assets/users/${this.selectedUser.avatar}`
+  // get imagePath() {
+  //   return `assets/users/${this.selectedUser.avatar}`
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]); //! using signal's set method
   }
 }
