60aa63565ef8589e305b01cba71ca5bca9b2d431
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-30 16:53:50 +0530

    feat: Added outputs and emitted data

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -13,4 +13,9 @@ import { DUMMY_USERS } from './dummy-users';
 })
 export class AppComponent {
   users = DUMMY_USERS;
+
+  //! When the user's click event is triggered, the onSelectUser method from user.component.ts is called. The emitted signal is then sent to the (select) event emitter. The (select) event then sends the emitted signal to the onSelectUser method of app.component.ts. The onSelectUser method from the app.component.ts then console.logs the selected user's id.
+  onSelectUser(id: string) {
+    console.log("Selected User with id: " + id);
+  }
 }
--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,23 +1,27 @@
-import { Component, computed, Input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, Input, input, Output, signal } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
-
 export class UserComponent {
   //! Input decorator is used to receive data from the parent component to this child component
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) id!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
 
+  //! Output decorator is used to send data from the child component to the parent component. In this case, it emits the id of the selected user. select is a custom named event
+  @Output() select = new EventEmitter();
+
+  //! This is a getter function
   get imagePath() {
-    return `assets/users/${this.avatar}`
+    return `assets/users/${this.avatar}`;
   }
 
   onSelectUser() {
-    
+    this.select.emit(this.id);
   }
 }
