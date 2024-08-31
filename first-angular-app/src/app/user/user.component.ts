8c8d3adbfc730bccb6c53db070e9d7b00fb6f902
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-31 18:22:52 +0530

    feat: Accepting Objects as Inputs & Adding Appropriate Types

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -9,19 +9,24 @@ import { Component, computed, EventEmitter, Input, input, Output, signal, output
 })
 export class UserComponent {
   //! Input decorator is used to receive data from the parent component to this child component
-  @Input({required: true}) id!: string;
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({required: true}) id!: string;
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+
+  //! Input decorator is used to receive json object from the parent component to this child component
+  @Input({required: true}) user!: {id: string, avatar: string, name: string};
 
   //! Output decorator is used to send data from the child component to the parent component. In this case, it emits the id of the selected user. select is a custom named event
   @Output() select = new EventEmitter<string>();
 
   //! This is a getter function
   get imagePath() {
-    return `assets/users/${this.avatar}`;
+    // return `assets/users/${this.avatar}`;
+    return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    // this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
