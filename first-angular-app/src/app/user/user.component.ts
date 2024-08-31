e702b3a775af2c0dae54befc4d52bff2ba13e58e
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-08-31 18:31:35 +0530

    Type Aliases and Inference

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, computed, EventEmitter, Input, input, Output, signal, output } from '@angular/core';
 
+// type User = {
+//   id: string,
+//   avatar: string,
+//   name: string
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,25 +20,19 @@ import { Component, computed, EventEmitter, Input, input, Output, signal, output
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  //! Input decorator is used to receive data from the parent component to this child component
-  // @Input({required: true}) id!: string;
-  // @Input({required: true}) avatar!: string;
-  // @Input({required: true}) name!: string;
-
   //! Input decorator is used to receive json object from the parent component to this child component
-  @Input({required: true}) user!: {id: string, avatar: string, name: string};
+  // @Input({required: true}) user!: {id: string, avatar: string, name: string};
+  @Input({required: true}) user!: User;
 
   //! Output decorator is used to send data from the child component to the parent component. In this case, it emits the id of the selected user. select is a custom named event
   @Output() select = new EventEmitter<string>();
 
   //! This is a getter function
   get imagePath() {
-    // return `assets/users/${this.avatar}`;
     return `assets/users/${this.user.avatar}`;
   }
 
   onSelectUser() {
-    // this.select.emit(this.id);
     this.select.emit(this.user.id);
   }
 }
