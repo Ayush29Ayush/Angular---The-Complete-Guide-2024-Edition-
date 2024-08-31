404032d691ea0478af5928b32fee78dad2f17eb5
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-09-01 00:08:38 +0530

    feat: Added task component + css styling

--- first-angular-app/src/app/tasks/tasks.component.ts
+++ first-angular-app/src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { TaskComponent } from "./task/task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
