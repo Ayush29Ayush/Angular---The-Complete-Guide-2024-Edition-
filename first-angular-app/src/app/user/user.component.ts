ae42b55042a1acdc65d935f99a1f604b46c11198
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-09-04 15:28:26 +0530

    feat: wrapped the app-card component around to get rounded borders

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, computed, EventEmitter, Input, input, Output, signal, output } from '@angular/core';
 import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
