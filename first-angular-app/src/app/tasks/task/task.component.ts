f498db6bfc29756c84211b1e70e2ae8e9cfbb395
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-09-05 19:00:26 +0530

    feat: Implemented the Complete task using services

--- first-angular-app/src/app/tasks/task/task.component.ts
+++ first-angular-app/src/app/tasks/task/task.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { type Task } from './task.model';
 import { CardComponent } from "../../shared/card/card.component";
+import { TasksService } from "../tasks.service";
 
 @Component({
   selector: 'app-task',
@@ -12,9 +13,12 @@ import { CardComponent } from "../../shared/card/card.component";
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task;
-  @Output() complete = new EventEmitter<string>();
+  // @Output() complete = new EventEmitter<string>();
+
+  private tasksService = inject(TasksService)
 
   onCompleteTask() {
-    this.complete.emit(this.task.id);
+    // this.complete.emit(this.task.id);
+    this.tasksService.removeTask(this.task.id);
   }
 }
