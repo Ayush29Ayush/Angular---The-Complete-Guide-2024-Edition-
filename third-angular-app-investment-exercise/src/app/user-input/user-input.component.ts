37754635fcbb3d9788e6e5c7c6d0ee5bc6617991
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-12-22 10:53:11 +0530

    feat: Handling Form Submissions

--- third-angular-app-investment-exercise/src/app/user-input/user-input.component.ts
+++ third-angular-app-investment-exercise/src/app/user-input/user-input.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  onSubmit() {
+    console.log('Form submitted!');}
 }
