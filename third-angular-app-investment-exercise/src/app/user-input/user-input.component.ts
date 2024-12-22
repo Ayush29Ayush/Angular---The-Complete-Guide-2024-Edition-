824d1dc470d85203a0bc0ba2d7cecb7aa77fa6a2
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-12-22 11:12:23 +0530

    feat: Extracting Values with Two-Way-Binding

--- third-angular-app-investment-exercise/src/app/user-input/user-input.component.ts
+++ third-angular-app-investment-exercise/src/app/user-input/user-input.component.ts
@@ -6,9 +6,21 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
   onSubmit() {
-    console.log('Form submitted!');}
+    console.log('Form submitted!');
+    console.log(
+      this.enteredInitialInvestment,
+      this.enteredAnnualInvestment,
+      this.enteredExpectedReturn,
+      this.enteredDuration
+    )
+  }
 }
