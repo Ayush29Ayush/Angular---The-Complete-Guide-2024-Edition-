7235224306390733fc741934f5c4e13064267a83
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-12-26 19:47:44 +0530

    feat: Using Signals in Services

--- third-angular-app-investment-exercise/src/app/investment-results/investment-results.component.ts
+++ third-angular-app-investment-exercise/src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject, Input, input } from '@angular/core';
+import { Component, computed, inject, Input, input } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -24,7 +24,12 @@ export class InvestmentResultsComponent {
   //! Approach 3: Use a service either by using constructor or by using dependency injection
   private investmentService = inject(InvestmentService);
 
-  get results() {
-    return this.investmentService.resultData;
-  }
+  //! Approach 1: Generic
+  // get results() {
+  //   return this.investmentService.resultData;
+  // }
+  //! Appraoch 2: Using signals with computed so that it is read-only
+  results = computed(() => this.investmentService.resultData());
+  //! Approach 3: Directly using signals
+  // results = this.investmentService.resultData.asReadonly();
 }
--- third-angular-app-investment-exercise/src/app/investment.service.ts
+++ third-angular-app-investment-exercise/src/app/investment.service.ts
@@ -1,16 +1,27 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import type { InvestmentInput } from './investment-input.model';
 
 @Injectable({ providedIn: 'root' })
 export class InvestmentService {
-  resultData?: {
-    year: number;
-    interest: number;
-    valueEndOfYear: number;
-    annualInvestment: number;
-    totalInterest: number;
-    totalAmountInvested: number;
-  }[];
+  // resultData?: {
+  //   year: number;
+  //   interest: number;
+  //   valueEndOfYear: number;
+  //   annualInvestment: number;
+  //   totalInterest: number;
+  //   totalAmountInvested: number;
+  // }[];
+  resultData = signal<
+    | {
+        year: number;
+        interest: number;
+        valueEndOfYear: number;
+        annualInvestment: number;
+        totalInterest: number;
+        totalAmountInvested: number;
+      }[]
+    | undefined
+  >(undefined);
 
   calculateInvestmentResults(data: InvestmentInput) {
     const { initialInvestment, duration, expectedReturn, annualInvestment } =
@@ -38,6 +49,8 @@ export class InvestmentService {
     // console.log(annualData);
     // this.resultsData = annualData;
     // this.resultsData.set(annualData);
-    this.resultData = annualData;
+
+    // this.resultData = annualData;
+    this.resultData.set(annualData);
   }
 }
