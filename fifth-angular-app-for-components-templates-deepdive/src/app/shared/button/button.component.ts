1a33fa0daade169aaeb4fd1cfb662217d6f6c2fa
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-09 00:20:29 +0530

    feat: Extending Built-in Elements with Custom Components via Attribute Selectors

---
+++ fifth-angular-app-for-components-templates-deepdive/src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector: 'app-button',
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
