a8f2ff15ccc79ae7af9e94b5fb9909f17961eeba
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-11 14:38:20 +0530

    feat: Configured View Encapsulation

--- fifth-angular-app-for-components-templates-deepdive/src/app/shared/control/control.component.ts
+++ fifth-angular-app-for-components-templates-deepdive/src/app/shared/control/control.component.ts
@@ -1,11 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class ControlComponent {
   label = input.required<string>()
