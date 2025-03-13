e9bc06130006a737b0b199c8d78323898dca91e0
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-13 22:08:19 +0530

    feat: Implementing Lifecycle Interfaces

--- fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/server-status/server-status.component.ts
+++ fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,8 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  // currentStatus = 'offline';
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline'; //! Setting specific string values as types uses a typescript feature called "literal Types". The idea is to only allow specific (string) values - instead of all strings.
 
   constructor() {}
