68421207547c5a00312c5144ef7a591634303a06
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-14 20:36:14 +0530

    feat: Angular Lifecycle hooks

--- fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,9 +10,14 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements AfterViewInit {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  ngAfterViewInit(): void {
+    console.log('AFTER VIEW INIT');
+    console.log(this.form().nativeElement);
+  }
 
   onSubmit(title: string, request: string) {
     console.log("The title is: " + title + ", and the request is: " + request);
