d55f6423bb3626ebc2cb6a8df28dec8e4ffc76f3
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-16 14:38:34 +0530

    feat: Added input and output signals

--- fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ fifth-angular-app-for-components-templates-deepdive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>()
+  detailsVisible = signal(false);
 
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+    this.detailsVisible.update(current => !current);
+  }
 }
