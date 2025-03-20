15c8b9096d315d4eb20935c0aab518ee4e87ea01
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-20 11:53:57 +0530

    feat: Analyzing attribute and structural directives

--- seventh-directives-deep-dive/src/app/app.component.ts
+++ seventh-directives-deep-dive/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 
 import { AuthComponent } from './auth/auth.component';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
+import { AuthService } from './auth/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  // imports: [AuthComponent, LearningResourcesComponent, NgIf],
   imports: [AuthComponent, LearningResourcesComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  private authService = inject(AuthService);
+
+  isAdmin = computed(() => this.authService.activePermission() ==='admin');
+
+  constructor() {
+    effect(()=> {
+      console.log(this.isAdmin())
+    })
+  }
+}
