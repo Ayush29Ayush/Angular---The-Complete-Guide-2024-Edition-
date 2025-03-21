a6fb45803c9dc2d3b15bdc1e3c0232a605a3e2bc
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-21 17:32:22 +0530

    feat: Using Attribute Directives To Change Element Behavior

--- seventh-directives-deep-dive/src/app/safe-link.directive.ts
+++ seventh-directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     // selector: 'app-safe-link' //! Tag Selector
@@ -9,6 +9,9 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirective {
+    queryParam = input('myapp', {alias: 'appSafeLink'});
+    private hostElementRef = inject<ElementRef<HTMLAnchorElement>>(ElementRef)
+
     constructor() {
         console.log("SafeLinkDirective is active!");
     }
@@ -17,6 +20,10 @@ export class SafeLinkDirective {
         const wantsToLeave = window.confirm("Do you want to leave the app?");
 
         if (wantsToLeave) {
+            // const address = (event.target as HTMLAnchorElement).href;
+            const address = this.hostElementRef.nativeElement.href;
+            // (event.target as HTMLAnchorElement).href = address + '?from=' + this.queryParam();
+            this.hostElementRef.nativeElement.href = address + '?from=' + this.queryParam();
             return;
         }
 
