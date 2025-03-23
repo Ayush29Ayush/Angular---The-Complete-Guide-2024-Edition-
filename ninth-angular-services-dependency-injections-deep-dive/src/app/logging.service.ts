f80ddf00c95dc5e80eab69eb45391c460a9fd31c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-23 20:27:15 +0530

    feat: Updated services to add logging functionality

---
+++ ninth-angular-services-dependency-injections-deep-dive/src/app/logging.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggingService {
+  log(message: string) {
+    const timeStamp = new Date().toLocaleDateString();
+    console.log(`${timeStamp}: ${message}`);
+  }
+  // constructor() { }
+}
--- ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks.service.ts
+++ ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, signal } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 
 import { Task, TaskStatus } from './task.model';
+import { LoggingService } from '../logging.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TasksService {
   private tasks = signal<Task[]>([]);
+  private loggingService = inject(LoggingService);
 
   allTasks = this.tasks.asReadonly();
 
@@ -17,6 +19,7 @@ export class TasksService {
       status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+    this.loggingService.log("Added task with title" + taskData.title);
   }
 
   updateTaskStatus(taskId: string, newStatus: TaskStatus) {
@@ -25,5 +28,6 @@ export class TasksService {
         task.id === taskId ? { ...task, status: newStatus } : task
       )
     );
+    this.loggingService.log("Updated task with id " + taskId);
   }
 }
