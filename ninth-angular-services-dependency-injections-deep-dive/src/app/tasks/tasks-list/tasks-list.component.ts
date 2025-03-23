c38caa3c46b01019552c4cc613ac926390f5fa0c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-03-23 16:42:55 +0530

    feat: Outsourcing & Reusing Logic with Services

--- ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
+++ ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -15,7 +15,19 @@ export class TasksListComponent {
   private tasksService = inject(TasksService);
   selectedFilter = signal<string>('all');
   // tasks = [];
-  tasks = this.tasksService.allTasks;
+  // tasks = this.tasksService.allTasks;
+  tasks = computed(() => {
+    switch (this.selectedFilter()) {
+      case 'open':
+        return this.tasksService.allTasks().filter((task) => task.status === 'OPEN');
+      case 'in-progress':
+        return this.tasksService.allTasks().filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService.allTasks().filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
--- ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks.service.ts
+++ ninth-angular-services-dependency-injections-deep-dive/src/app/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +14,16 @@ export class TasksService {
     const newTask: Task = {
       ...taskData,
       id: Math.random().toString(),
-      status: 'OPEN'
+      status: 'OPEN',
     };
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
