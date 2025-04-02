af38024e927e43a61365e809b8582744c5a876ea
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-02 23:17:20 +0530

    feat: Working with Signals

--- eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
+++ eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -7,16 +7,30 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
   private destroyRef = inject(DestroyRef);
-  ngOnInit(): void {
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe({
-        next: (val) => console.log(val),
-      });
 
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
+  constructor() {
+    effect(() => {
+      console.log(`Click button clicked ${this.clickCount()} times.`);
+    })
+  }
+
+  ngOnInit(): void {
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe({
+    //     next: (val) => console.log(val),
+    //   });
+
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+  }
+
+  onClick() {
+    // console.log('Control clicked!');
+    // this.clickCount.set(this.clickCount() + 1);
+    this.clickCount.update((val) => val + 1);
   }
 }
