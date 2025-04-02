5317658b0057d126c9ccc2e98771209c7d70b583
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-02 22:29:38 +0530

    feat: Creating & using an observable

--- eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
+++ eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+  ngOnInit(): void {
+    const subscription = interval(1000).subscribe({
+      next: (val) => console.log(val),
+    });
 
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
+  }
 }
