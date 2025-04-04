22c5c6db319a94b76ac253a9c68eece1105a8220
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-04 23:30:12 +0530

    feat: Converting Observable to Signal

--- eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
+++ eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -8,12 +9,13 @@ import { interval, map } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
   private destroyRef = inject(DestroyRef);
 
   constructor() {
-    effect(() => {
-      console.log(`Click button clicked ${this.clickCount()} times.`);
-    })
+    // effect(() => {
+    //   console.log(`Click button clicked ${this.clickCount()} times.`);
+    // })
   }
 
   ngOnInit(): void {
@@ -26,6 +28,13 @@ export class AppComponent implements OnInit {
     // this.destroyRef.onDestroy(() => {
     //   subscription.unsubscribe();
     // });
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`Click button clicked ${val} times.`),
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    });
   }
 
   onClick() {
