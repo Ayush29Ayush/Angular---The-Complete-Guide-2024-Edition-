f9eddf54e6e12572cc6dbc924017e9a24cc29a7f
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-04 23:44:39 +0530

    feat: Converting Observables to Signals

--- eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
+++ eleventh-angular-rxjs-observables-deep-dive/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, DestroyRef, effect, inject, OnInit, signal } from '@angular/core';
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { interval, map } from 'rxjs';
 
 @Component({
@@ -10,6 +10,8 @@ import { interval, map } from 'rxjs';
 export class AppComponent implements OnInit {
   clickCount = signal(0);
   clickCount$ = toObservable(this.clickCount);
+  interval$ = interval(1000);
+  intervalSignal = toSignal(this.interval$, { initialValue: 0 });
   private destroyRef = inject(DestroyRef);
 
   constructor() {
