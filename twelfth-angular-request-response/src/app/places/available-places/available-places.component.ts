83b2eafccb8047026bd8354cae89b8afc2a90218
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-05 11:31:35 +0530

    feat: Transforming & Using Response Data

--- twelfth-angular-request-response/src/app/places/available-places/available-places.component.ts
+++ twelfth-angular-request-response/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -23,9 +24,11 @@ export class AvailablePlacesComponent implements OnInit {
     //! Define the type and shape of the response
     const subscription = this.httpClient
       .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(map((resData) => resData.places))
       .subscribe({
-        next: (resData) => {
-          console.log(resData.places);
+        next: (places) => {
+          console.log(places);
+          this.places.set(places);
         },
       });
 
