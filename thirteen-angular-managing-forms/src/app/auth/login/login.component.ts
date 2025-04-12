82b69cb59d33543733bbed9f25b92bc8d29d8263
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-12 19:18:50 +0530

    feat: Using the Form Validation Status To Provide User Feedback

--- thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
+++ thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
@@ -11,10 +11,13 @@ import { FormsModule, NgForm } from '@angular/forms';
 export class LoginComponent {
   onSubmit(formData: NgForm) {
     console.log(formData.form);
+    if (formData.form.invalid) {
+      console.log("Form is invalid due to angular validators added in the template.")
+      return;
+    }
     console.log(formData.form.value);
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
-
     console.log(`Email: ${enteredEmail}, Password: ${enteredPassword}`);
   }
 }
