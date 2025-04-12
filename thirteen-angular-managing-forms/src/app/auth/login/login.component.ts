dc2d1dcaece8813a3821039ad892695b5d734812
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-12 18:15:28 +0530

    feat: Extracting User Input Values from Template Driven Angular Managed Form

--- thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
+++ thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmit(formData: NgForm) {
+    console.log(formData.form);
+    console.log(formData.form.value);
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+
+    console.log(`Email: ${enteredEmail}, Password: ${enteredPassword}`);
+  }
+}
