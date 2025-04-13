c3a60aef28a26941ea99bb56485a75aca0efe597
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-13 12:26:32 +0530

    feat: Interacting With The Underlying Form Object In The Component

--- thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
+++ thirteen-angular-managing-forms/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {afterNextRender,Component,DestroyRef,inject,viewChild} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,15 +10,47 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+      
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        // this.form().setValue({
+        //   email: savedEmail,
+        //   password: ''
+        // })
+        setTimeout(()=>{this.form().controls['email'].setValue(savedEmail);}, 10);
+      }
+
+      const formRef = this.form();
+      const subscription = formRef?.valueChanges?.pipe(debounceTime(500)).subscribe({
+        // next: (value) => console.log(value)
+        next: (value) =>
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          ),
+      });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
     console.log(formData.form);
     if (formData.form.invalid) {
-      console.log("Form is invalid due to angular validators added in the template.")
+      console.log('Form is invalid due to angular validators added in the template.');
       return;
     }
     console.log(formData.form.value);
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
     console.log(`Email: ${enteredEmail}, Password: ${enteredPassword}`);
+
+    formData.form.reset();
   }
 }
