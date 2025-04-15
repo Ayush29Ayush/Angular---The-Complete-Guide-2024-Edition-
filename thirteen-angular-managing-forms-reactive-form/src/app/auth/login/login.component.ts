6825704d5f48895704501c3e36d87f9f797f6617
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-15 17:59:26 +0530

    feat: Reactive Forms Project Setup

---
+++ thirteen-angular-managing-forms-reactive-form/src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  })
+}
