acd3f1f1aabe87fc93a926ff5c7a5796c3aaf721
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-17 10:05:25 +0530

    feat: Handling Form Submission (Reactive Forms) + Adding Validators To Reactive Forms

--- thirteen-angular-managing-forms-reactive-form/src/app/auth/login/login.component.ts
+++ thirteen-angular-managing-forms-reactive-form/src/app/auth/login/login.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
   form = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
   })
+
+  get emailIsInvalid() {
+    return this.form.controls['email'].invalid && this.form.controls['email'].touched && this.form.controls['email'].dirty;
+  }
+
+  get passwordIsInvalid() {
+    return this.form.controls['password'].invalid && this.form.controls['password'].touched && this.form.controls['password'].dirty;
+  }
+
+  onSubmit() {
+    // console.log(this.form);
+    // console.log(this.form.value);
+
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(`Email: ${enteredEmail}, Password: ${enteredPassword}`);
+  }
 }
