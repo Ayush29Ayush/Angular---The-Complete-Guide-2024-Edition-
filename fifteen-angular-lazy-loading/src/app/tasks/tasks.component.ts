14a8d945f4b96e17896dcdc4cfcf460a40df638c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-04-27 12:39:25 +0530

    feat: Implementing Route Based Lazy Loading

--- fifteen-angular-lazy-loading/src/app/tasks/tasks.component.ts
+++ fifteen-angular-lazy-loading/src/app/tasks/tasks.component.ts
@@ -21,24 +21,3 @@ export class TasksComponent {
   userId = input.required<string>();
   order = input<'asc' | 'desc' | undefined>();
 }
-
-export const resolveUserTasks: ResolveFn<Task[]> = (
-  activatedRouteSnapshot,
-  routerState
-) => {
-  const order = activatedRouteSnapshot.queryParams['order'];
-  const tasksService = inject(TasksService);
-  const tasks = tasksService
-    .allTasks()
-    .filter(
-      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-    );
-
-  if (order && order === 'asc') {
-    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-  } else {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-  }
-
-  return tasks.length ? tasks : [];
-};
--- fifteen-angular-lazy-loading/src/app/users/users.routes.ts
+++ fifteen-angular-lazy-loading/src/app/users/users.routes.ts
@@ -1,7 +1,31 @@
-import { Routes } from '@angular/router';
+import { ResolveFn, Routes } from '@angular/router';
 
-import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
+// import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent, canLeaveEditPage } from '../tasks/new-task/new-task.component';
+import { Task } from '../tasks/task/task.model';
+import { inject } from '@angular/core';
+import { TasksService } from '../tasks/tasks.service';
+
+const resolveUserTasks: ResolveFn<Task[]> = (
+  activatedRouteSnapshot,
+  routerState
+) => {
+  const order = activatedRouteSnapshot.queryParams['order'];
+  const tasksService = inject(TasksService);
+  const tasks = tasksService
+    .allTasks()
+    .filter(
+      (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
+    );
+
+  if (order && order === 'asc') {
+    tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
+  } else {
+    tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
+  }
+
+  return tasks.length ? tasks : [];
+};
 
 export const routes: Routes = [
   {
@@ -11,7 +35,8 @@ export const routes: Routes = [
   },
   {
     path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
+    // component: TasksComponent,
+    loadComponent: () => import('../tasks/tasks.component').then(mod => mod.TasksComponent),
     runGuardsAndResolvers: 'always',
     resolve: {
       userTasks: resolveUserTasks,
