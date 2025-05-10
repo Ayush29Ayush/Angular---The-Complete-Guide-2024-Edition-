9b780a57a1d49261f3df37e481908ccd27f8dc1c
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-05-10 15:23:40 +0530

    feat: Rendering Routes

--- fourteen-angular-routing/src/app/app.routes.ts
+++ fourteen-angular-routing/src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { TaskComponent } from './tasks/task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 export const routes: Routes = [
   {
     path: 'tasks', // <your-domain>/tasks
-    component: TaskComponent,
+    component: TasksComponent,
   },
 ];
