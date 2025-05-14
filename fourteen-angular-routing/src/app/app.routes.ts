b0bb266b2c8092b0b4e20cc7339f5f5769e28188
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-05-14 17:20:51 +0530

    feat: Nested Routes added

--- fourteen-angular-routing/src/app/app.routes.ts
+++ fourteen-angular-routing/src/app/app.routes.ts
@@ -1,22 +1,27 @@
 import { Routes } from '@angular/router';
+
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
     path: '', // <your-domain>/
     component: NoTaskComponent,
-    // redirectTo: '/users/u1',
-    // pathMatch: 'full',
-    title: 'No task selected',
   },
   {
     path: 'users/:userId', // <your-domain>/users/<uid>
     component: UserTasksComponent,
-  },
-  {
-    path: 'tasks', // <your-domain>/tasks
-    component: TasksComponent,
-  },
+    children: [
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent
+      }
+    ]
+  }
 ];
