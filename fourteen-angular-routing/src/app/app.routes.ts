c6e1a42aa1254061660d4e10dbf2cd9b48d31ec4
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2025-05-14 12:18:56 +0530

    feat: Registering and Rendering Multiple Routes + Styling Active Navigation Links + Setting Up & Navigating To Dynamic Routes

--- fourteen-angular-routing/src/app/app.routes.ts
+++ fourteen-angular-routing/src/app/app.routes.ts
@@ -1,7 +1,20 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
+  {
+    path: '', // <your-domain>/
+    component: NoTaskComponent,
+    // redirectTo: '/users/u1',
+    // pathMatch: 'full',
+    title: 'No task selected',
+  },
+  {
+    path: 'users/:userId', // <your-domain>/users/<uid>
+    component: UserTasksComponent,
+  },
   {
     path: 'tasks', // <your-domain>/tasks
     component: TasksComponent,
--- fourteen-angular-routing/src/app/users/user/user.component.ts
+++ fourteen-angular-routing/src/app/users/user/user.component.ts
@@ -1,10 +1,12 @@
 import { Component, computed, input } from '@angular/core';
 
 import { type User } from './user.model';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
